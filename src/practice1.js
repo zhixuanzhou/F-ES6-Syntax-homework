aae93eb36b29b59d7e8ed318850b14eb5ad2a998
zhixuan zhou <zhixuan.zhou@zhixuans-MacBook-Pro.local>
2020-11-10 21:10:30 +0800

    finish practice 1 and practice 2

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,10 @@
-const parseData
+const parseData = input => {
+    return input.data.map( i => {
+        const out = {};
+        input.column.forEach( (x, z) => {
+          out[x.name] = i[z];
+        })
+        return out;
+    });
+}
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,12 @@
-const inject
+const inject = (items, sections) => {
+    const out = [];
+    items.forEach((i , x) => {
+        const z = sections.find(e => e.index === x);
+        if (z){
+            out.push(z.content)
+        };
+        out.push(i);
+    });
+    return out;
+}
 export { inject };
